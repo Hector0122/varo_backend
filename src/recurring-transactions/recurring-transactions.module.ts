fb852d6e496b89d6e3e310f5e5b9ad0789f0e047
Hector Paramo <hector.paramo@g-global.com>
2026-06-12 16:35:01 -0700

    feat: add ForecastModule import to RecurringTransactionsModule

--- src/recurring-transactions/recurring-transactions.module.ts
+++ src/recurring-transactions/recurring-transactions.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ForecastModule } from '../forecast/forecast.module';
 import { RecurringTransactionsService } from './recurring-transactions.service';
 import { RecurringTransactionsController } from './recurring-transactions.controller';
 
 @Module({
+  imports: [ForecastModule],
   controllers: [RecurringTransactionsController],
   providers: [RecurringTransactionsService],
   exports: [RecurringTransactionsService],
