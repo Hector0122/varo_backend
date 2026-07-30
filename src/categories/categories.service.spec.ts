577eccfe9b15efd40ec192d31bdc22b2065c6bde
Hector Paramo <hector.paramo@g-global.com>
2026-07-30 12:20:59 -0700

    test: update CategoriesController and CategoriesService tests with proper service mock

--- src/categories/categories.service.spec.ts
+++ src/categories/categories.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesService } from './categories.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('CategoriesService', () => {
   let service: CategoriesService;
 
   beforeEach(async () => {
+    const prisma = {
+      category: {
+        findMany: jest.fn(),
+        findFirst: jest.fn(),
+        findUnique: jest.fn(),
+        create: jest.fn(),
+        delete: jest.fn(),
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoriesService],
+      providers: [
+        CategoriesService,
+        { provide: PrismaService, useValue: prisma },
+      ],
     }).compile();
 
     service = module.get<CategoriesService>(CategoriesService);
